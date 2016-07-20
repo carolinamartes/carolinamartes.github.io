0d8410662890a67252c5f9331b6ec52461e8f2a4
Carolina Martes <carolina.martes@gmail.com>
2016-07-20 19:37:19 -0400

    First commit

---
+++ key.js
@@ -0,0 +1,25 @@
+var English1Left=
+"If you surrendered to the air, you could ride it. -Toni Morrison";
+
+var English1Right=
+"It was a bright cold day in April, and the clocks were striking thirteen. -Orwell";
+
+var English2Left=
+"We real cool. We Left school. We Lurk late. We Strike straight. We Sing sin. We Thin gin. We Jazz June. We Die soon. -Gwendolyn Brooks" ;
+
+var English2Right=
+"It sounds plausible enough tonight, but wait until tomorrow. Wait for the common sense of the morning. -H.G Wells" ;
+
+var Spanish1Left= "El que lee mucho y anda mucho, ve mucho y sabe mucho. -Cervantes"
+
+var Spanish1Right="Siempre imaginé que el Paraíso sería algún tipo de biblioteca. -Borges"
+
+var Spanish2Right="Es cualquier libro discreto (que si cansa, de hablar deja) un amigo que aconseja y que reprende en secreto. -Lope de Vega"
+
+var French1Right= "C'est cela l'amour, tout donner, tout sacrifier sans espoir de retour. -Camus";
+
+var French1Left= "Le coeur humain, beau comme un sismographe. -Breton";
+
+var French2Right= "La densité de l'Histoire ne détermine aucun de mes actes. Je suis mon propre fondement. Et c'est en dépassant la donnée historique, instrumentale, que j'introduis le cycle de ma liberté. -Fanon"
+
+var French2Left="Chaque atome d'un bruit dûr. Exhale l'arome d'un' fruit mûr. -Ana Claudia Antunes"
